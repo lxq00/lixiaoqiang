bc1843c27a2bd7df8b016f0b40e5f2a8a76cf98d
方秋梓 <lxq00@foxmail.com>
2018-06-17 22:54:01 +0800

    add live555 by rtsp

---
+++ cpp/public/Include/RTSP/Defs.h
@@ -0,0 +1,16 @@
+#ifndef __PUBLIC_RTSP_H__
+#define __PUBLIC_RTSP_H__
+#include "RTSPStructs.h"
+
+#ifdef WIN32
+	#ifdef RTSP_EXPORTS
+		#define RTSP_API __declspec(dllexport)
+	#else
+		#define RTSP_API __declspec(dllimport)
+	#endif
+#else
+	#define RTSP_API
+#endif
+
+
+#endif
